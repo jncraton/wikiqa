cbd93b10a51c062b3a0bb0a666d8a7e291decb58
Jon Craton <jncraton@gmail.com>
2022-12-18 10:45:55 -0500

    Adjust long line

--- qa.py
+++ qa.py
@@ -232,7 +232,10 @@ if __name__ == "__main__":
 
     while True:
         # Instruction for a chitchat task
-        instruction = "Instruction: given a dialog context and related knowledge, you need to answer the question based on the knowledge."
+        instruction = (
+            "Instruction: given a dialog context and related knowledge, "
+            "you need to answer the question based on the knowledge."
+        )
         # Leave the knowldge empty
         query = input("You: ")
         dialog.append(query)
